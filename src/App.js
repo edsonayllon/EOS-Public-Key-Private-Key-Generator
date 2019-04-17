e860bf28768e4c3ad5cc49373486668c65c55d1b
eayllon1 <eayllon1@gmail.com>
2019-04-16 21:45:10 -0400

    generates public private key pairings

--- src/App.js
+++ src/App.js
@@ -1,12 +1,41 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import ecc from 'eosjs-ecc';
+import Button from './components/Button';
 
 export default class App extends React.Component {
+
+  state = {
+    publicKey: '',
+    privateKey: '',
+    loaded: false
+  }
+
+  generateKeyPairing = async () => {
+    var privkey = await ecc.randomKey();
+    var pubkey = await ecc.privateToPublic(privkey);
+    this.setState({
+      loaded: true,
+      publicKey: pubkey,
+      privateKey: privkey
+    })
+  }
+
   render() {
     return (
       <View style={styles.container}>
-        <Text style = {{fontSize: 20}}>EOS Pulic Key Private Key Generation</Text>
-
+        <Text style = {{fontSize: 20, padding: 20}}>EOS Pulic Key Private Key Generation</Text>
+        <Button
+          title='Generate Key Pairing'
+          onPress={this.generateKeyPairing}
+         />
+        { this.state.loaded ? (
+            <View>
+              <Text style={{ marginBottom: 12, marginTop: 12}}>Public Key: {this.state.publicKey}</Text>
+              <Text>Private Key: {this.state.privateKey}</Text>
+            </View>
+          ) : null
+        }
       </View>
     );
   }
